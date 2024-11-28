ab415c037141496e7e2610306f8eaf547c6178d5
Huy Joseph Nguyen <huy.nd>
2024-11-28 02:54:18 +0000

    push button print pdf

--- webapp/ext/controller/PrintPDF.controller.js
+++ webapp/ext/controller/PrintPDF.controller.js
@@ -14,6 +14,9 @@ sap.ui.define(
 	let strXml = "";
 	var limit = 10;
 
+	var username = "CPM_USER";  
+	var password = "pscqTSJ5}vvYWqnbnTFfhyUFjegjonBdGDiiqpvM";
+
 	return ControllerExtension.extend('ns1.zfi1002v3.ext.controller.PrintPDF', {
 		// this section allows to extend lifecycle hooks or hooks provided by Fiori elements
 		override: {
@@ -25,8 +28,8 @@ sap.ui.define(
 			onInit: function () {
 				// you can access the Fiori elements extensionAPI via this.base.getExtensionAPI
 				var oModel = this.base.getExtensionAPI().getModel();
-				// this.getOAuthToken();
-				// this.onOpenPdf(base64PDF);
+
+				// this.callApiWithToken("a");
 			},
 		},
 		PrintPDF: async function() {
@@ -105,7 +108,7 @@ sap.ui.define(
 					'AmountInWords':'',
 					'SUM': this.formatMoney(arrTable.map(o => o.DEBIT).reduce((a, c) => { return a + c }), arrHe.TransactionCurrency),
 				}
-				console.log("obj", obj);
+				// console.log("obj", obj);
 			await jQuery.get(this._sValidPath, function(xml_string) {
 				var parser = new DOMParser();
 				var xmlDoc = parser.parseFromString(xml_string, 'application/xml');  
@@ -138,7 +141,7 @@ sap.ui.define(
 				console.error("Error:", textStatus, errorThrown);
 			});
 
-			console.log("strXml", strXml);
+			// console.log("strXml", strXml);
 			return strXml;
 		},
 		filterLineByHeader: function(arrAccD, oBindList) {
@@ -172,17 +175,13 @@ sap.ui.define(
 
 			let htmlElement = "";
 			for (let k = 0; k < oPagebreakLine.length; k++) {
-				// console.log("oPagebreakLine[k]", oPagebreakLine[k]);
 				htmlElement += await this.jsonToXml(oPagebreakLine[k]);
-				
 			}
 			
 			// In ra pdf
 			 xmlStr = '<Root>' + htmlElement + '</Root>';
 			console.log("xmlStr", xmlStr);
-			base64String = btoa(unescape(encodeURIComponent(xmlStr)));
-			console.log("encodedString",base64String);
-			setTimeout(() => this.getOAuthToken(base64String, true), 3000);
+			setTimeout(() => this.callApiWithToken(xmlStr, true), 3000);
 		},
 		nestArrays: async function(headerItem, lineItems) {
 			const lineItemsMap = {};
@@ -246,94 +245,32 @@ sap.ui.define(
 				pdfWindow = window.open(pdfUrl);
 			}
 		},
-		getOAuthToken: async function(base64String){
-			// console.log("base64String base64String", base64String);
-			var tokenUrl = 'https://training-integration-xzunlbtw.authentication.us10.hana.ondemand.com/oauth/token'; 
-			var clientId = 'sb-70736f9d-018b-427c-bc90-37be93976825!b308585|ads-xsappname!b65488';
-			var clientSecret = 'ecb31903-21a7-4935-a29a-cb5d9f7352c5$dLSusXV9ZfFT5gK8fKLqpS8lp3lbfpALnShWBIniqxQ=';
-			var grantType = 'client_credentials'; 
-
-			var postData = {
-				grant_type: grantType,
-				client_id: clientId,
-				client_secret: clientSecret
-			};
-			
-			jQuery.ajax({
-				url: tokenUrl,
-				type: 'POST',
-				contentType: 'application/x-www-form-urlencoded',
-				data: postData,
-				success: function(response) {
-					// Lưu access token để sử dụng cho các request tiếp theo
-					var accessToken = response.access_token;
-					// console.log('Token received: ', accessToken);
-					this.callApiWithToken(accessToken, base64String);
-				}.bind(this),
-				error: function(xhr, status, error) {
-					console.error('Error getting OAuth token:', error);
-				}
-			});
-		},
-		callApiWithToken:async function(accessToken, base64String){
-			// console.log("accessToken", accessToken);
-			var apiUrl = 'https://adsrestapi-formsprocessing.cfapps.us10.hana.ondemand.com/v1/adsRender/pdf?templateSource=storageName&TraceLevel=2'; 
+		callApiWithToken:async function(xml){
+			console.log("xml", xml);
 			var bodyData = {
-				"xdpTemplate": "Sales_Receipt/salesreceipt",
-				xmlData: base64String,
-				// "xmlData": "PFJvb3Q+PERldGFpbD4KCQkJPENvbXBhbnk+CgkJCQk8Q29tcGFueU5hbWU+VERJPC9Db21wYW55TmFtZT4gIAoJCQkJPFJlZ2lzdHJhdGlvbj48L1JlZ2lzdHJhdGlvbj4JCgkJCQk8QWRkcmVzcz4jI0FkZHJlc3MjIzwvQWRkcmVzcz4KCQkJCTxQaG9uZT4jI1Bob25lIyM8L1Bob25lPiAgCQkJCgkJCQk8VGF4Q29kZT4jI1RheENvZGUjIzwvVGF4Q29kZT4gICAKCQkJCTxFLW1haWw+IyNtYWlsIyM8L0UtbWFpbD4gIAoJCQkJPFdlYnNpdGU+IyN3ZWJzaXRlIyM8L1dlYnNpdGU+IAoJCQk8L0NvbXBhbnk+CgkJCTxJbnZvaWNlPgoJCQkJPFJlY2VpdmVkRnJvbT4jI1JlY2VpdmVkRnJvbSMjPC9SZWNlaXZlZEZyb20+ICAgCgkJCQk8VGVsPjAzNDIwNzQyNDYyNDwvVGVsPiAgCgkJCQk8RmF4PiA8L0ZheD4gIAoJCQkJPFJlY2VpcHRObz4xMDAwMDAzNzY8L1JlY2VpcHRObz4gIAoJCQkJPEJhbmtBY2NvdW50PjE4MDAxMTwvQmFua0FjY291bnQ+ICAKCQkJCTxDdXJyZW5jeT4xMDAwMDAzNzY8L0N1cnJlbmN5PiAgCgkJCQk8UHJvZHVjdHM+CgkJCQkJCQk8UHJvZHVjdD4KCQkJCQkJCQk8UmVjZWlwdERhdGUvPgoJCQkJCQkJCTxSZWZlcmVuY2U+RlggVW5yZWFsaXplZCBSZW1lYXN1cmVtZW50IEdhaW4uPC9SZWZlcmVuY2U+CgkJCQkJCQkJPEFtb3VudD4yMTIwMDAwMDwvQW1vdW50PgoJCQkJCQkJPC9Qcm9kdWN0PgoJCQkJCQksCgkJCQkJCQk8UHJvZHVjdD4KCQkJCQkJCQk8UmVjZWlwdERhdGUvPgoJCQkJCQkJCTxSZWZlcmVuY2UvPgoJCQkJCQkJCTxBbW91bnQ+NzI1NDk5OTk8L0Ftb3VudD4KCQkJCQkJCTwvUHJvZHVjdD4KCQkJCQkJIDwvUHJvZHVjdHM+CgkJCQk8QW1vdW50SW5Xb3Jkcz5N4buZdCB0cmnhu4d1IGJhIHRyxINtIG5naMOsbiDEkeG7k25nIGNo4bq1bjwvQW1vdW50SW5Xb3Jkcz4KCQkJCTxUb3RhbD4xMjAzNzA0PC9Ub3RhbD4gICAgICAKCQkJCTxOb3RlPlRoaXMgaXMgY29tcGFueTwvTm90ZT4gCgkJCTwvSW52b2ljZT4KCQk8L0RldGFpbD48L1Jvb3Q+",
-				"formType": "print",
-				"formLocale": "en_US",
-				"taggedPdf": 1,
-				"embedFont": 0,
-				"changeNotAllowed": false,
-				"printNotAllowed": false
+				"filecontentxml": xml,
+				"comm_scenario":"ZCS_FI1002A_GLVOUCHER",
+				"comm_system_id":"BTP_VAS",
+				"service_id":"ZFI10_02_GENERALLEDGERVOUCHER_OB_REST",
+				"queuename":"DEV_PRINT_QUEUE",
+				"documentname":"Test"
 			}
-			var proxyUrl = 'https://cors-anywhere.herokuapp.com/';
-			// jQuery.ajax({
-			// 	url: proxyUrl  + apiUrl,
-			// 	// url: apiUrl,
-			// 	type: 'POST',
-			// 	contentType: 'application/json',
-			// 	headers: {
-			// 		'Authorization': 'Bearer ' + accessToken ,
-			// 		'Access-Control-Allow-Origin': '*',
-			// 		'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE',
-			// 		'Access-Control-Allow-Headers': 'Content-Type, Authorization, Origin, X-Requested-With, Content-Type, Accept',
-			// 		'Access-Control-Allow-Credentials': true
-			// 	},
-			// 	data: JSON.stringify(bodyData),
-			// 	success: function(response) {
-			// 		console.log('API response: ', response);
-			// 		this.onOpenPdf(response.fileContent);
-			// 	}.bind(this),
-			// 	error: function(xhr, status, error) {
-			// 		console.error('Error calling API:', error);
-			// 	}
-			// });
 
-			const { destinationService } = require('@sap-cloud-sdk/core');
-			const fetch = require('node-fetch'); 
-			
-			const destination = await destinationService.getDestination('your-destination-name');
-
-			// Lấy URL và authentication từ destination
-			const apiUrl = destination.url;
-			const authHeader = destination.authentication === 'OAuth2' 
-								? await getOAuth2Token(destination) // Lấy OAuth2 token nếu cần
-								: null;
-
-			// Gọi API
-			const response = await fetch(apiUrl, {
-			method: 'GET',  // Phương thức gọi API
-			headers: {
-				'Authorization': accessToken ? `Bearer ${accessToken}` : '', // Token OAuth2 nếu cần
-				'Content-Type': 'application/json',
-			}
-			});
-
-			const data = await response.json();
-			console.log('API Response:', data);
+			var credentials = btoa(username + ":" + password);
+			var settings = {
+				"url": "/sap/bc/http/sap/zbtp_http_example",
+				"method": "POST",
+				"timeout": 0,
+				"headers": {
+				  "Content-Type": "application/json",
+				  "Authorization": "Basic " + credentials,
+				},
+				"data": JSON.stringify(bodyData)
+			  };
+			  
+			  jQuery.ajax(settings).done(function (response) {
+				console.log("Dữ liệu từ ABAP",response);
+			  });
 		},
 		formatDate(date) {
 			var dateIn = new Date(date);
